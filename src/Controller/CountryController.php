cce85667bf93e8bf302c8ea22b2b3a4c7da99643
André Sünnemann <a.suennemann@edv-peuker.de>
2020-03-22 01:21:46 +0100

    api request GET /api/country/list ok

---
+++ src/Controller/CountryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use App\Entity\Country;
+
+/**
+ * Class CountryController
+ * @package App\Controller
+ *
+ * @Route("/api", name="api__")
+ */
+class CountryController extends FOSRestController
+{
+    /**
+     * * @Rest\Get("/country/list")
+     */
+    public function listAction()
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $countries = $manager->getRepository('App:Country')->findAll();
+        return $this->handleView($this->view($countries));
+    }
+}
